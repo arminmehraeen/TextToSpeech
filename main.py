8eac650459a70b5b10daf8bc40884cc2317594e4
Armin Mehraeen <armin.mehraeen@gmail.com>
2022-06-03 00:12:23 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyttsx3
+
+
+def convert_text_to_speech(text):
+    sound = pyttsx3.init()
+    sound.setProperty('rate', 150)
+    sound.say(text=text)
+    sound.runAndWait()
+
+
+if __name__ == '__main__':
+    while True:
+        text = input('Enter Text : ')
+        convert_text_to_speech(text)
+
